d0caa52508b9e7bcde054426c36ca6eca42a60f8
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-01-22 19:14:44 +0100

    fix: linting setup for tailwind

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,12 @@
-import React from "react"
+import React from "react";
 
 export const Hero: React.FC = () => {
     return (
-
-        <div className="h-full w-full mb-4">
-            <video className="inset-0 z-1" autoPlay loop>
+        <div className="mb-4 h-full w-full">
+            <video className="z-1 inset-0" autoPlay loop>
                 <source src="./bg2.mp4" />
                 <p>Your browser does not support the video tag.</p>
             </video>
         </div>
-
-    )
-}
+    );
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,31 @@
-import { Pages } from '@/constants/pagesConst'
-import { BlueButton } from '@/UI/BlueButton'
-import React from 'react'
-import { NavLink } from './NavLink'
+import { Pages } from "@/constants/pagesConst";
+import { BlueButton } from "@/UI/BlueButton";
+import React from "react";
+import { NavLink } from "./NavLink";
 
 export const Navbar: React.FC = () => {
     return (
-        <nav className="bg-transparent top-0 left-0 right-0 z-10 absolute">
-            <div className="container flex flex-wrap items-end justify-between mx-auto">
-
-                <div className="flex md:order-2 items-end">
+        <nav className="absolute top-0 left-0 right-0 z-10 bg-transparent">
+            <div className="container mx-auto flex flex-wrap items-end justify-between">
+                <div className="flex items-end md:order-2">
                     <BlueButton link="/påmelding" text="Bli Medlem!" />
                 </div>
 
-
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                    <ul className="flex flex-col p-4 mt-4 border md:flex-row md:space-x-40 md:mt-0 md:text-sm md:font-medium md:border-0 ">
-                        {Pages.map((page, index) => (<NavLink key={index} link={page.link} text={page.text} />))}
+                <div
+                    className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
+                    id="navbar-cta"
+                >
+                    <ul className="mt-4 flex flex-col border p-4 md:mt-0 md:flex-row md:space-x-40 md:border-0 md:text-sm md:font-medium ">
+                        {Pages.map((page, index) => (
+                            <NavLink
+                                key={index}
+                                link={page.link}
+                                text={page.text}
+                            />
+                        ))}
                     </ul>
                 </div>
             </div>
-
         </nav>
-    )
-}
+    );
+};
