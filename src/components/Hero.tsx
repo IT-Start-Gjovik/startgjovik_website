ceb8dcabfb8ad9e09987ac20daaa96b1b03596fe
KjetilMainPc <kjetilindrehus@gmail.com>
2023-01-26 20:59:01 +0100

    add: temp main page content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Hero: React.FC = () => {
     return (
-        <div className="mb-4 h-full w-full">
-            <video className="z-1 inset-0" autoPlay loop>
+        <div className="h-full w-full">
+            <video className="z-1 inset-0" autoPlay={true} loop>
                 <source src="./bg2.mp4" />
                 <p>Your browser does not support the video tag.</p>
             </video>
