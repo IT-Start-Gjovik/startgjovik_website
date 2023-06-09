a74b0339895e398dbfcbc4f6f4434d21d7ca0ccc
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-06-09 15:46:16 +0200

    add: footer first draft

---
+++ components/footer/footer.tsx
@@ -0,0 +1,26 @@
+// Footer component 
+
+import Link from "next/link";
+
+export default function Footer(){
+
+    const licenseURL: string = "#";
+    const privacyPolicyURL: string = "#";
+
+    return (
+    <footer className="rounded-lg shadow m-4 bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span className="text-sm sm:text-center text-gray-400">© 2023 <Link href="https://startgjovik.no/" className="hover:underline">Start Gjøvik</Link>. Alle Rettigheter Reservert.
+            </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <Link href={privacyPolicyURL} className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
+                </li>
+                <li>
+                    <Link href={licenseURL} className="mr-4 hover:underline md:mr-6">Licensing</Link>
+                </li>
+            </ul>
+        </div>
+    </footer>
+    );
+}
