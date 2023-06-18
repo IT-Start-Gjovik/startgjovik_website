8671b2c7207d84f8e64319a333784ec30def5eda
Kjetil Indrehus <kjetil.indrehus@bekk.no>
2023-06-18 16:26:45 +0200

    add: button for going back + refactor buttons to components

--- app/(site)/events/[eventPage]/page.tsx
+++ app/(site)/events/[eventPage]/page.tsx
@@ -6,6 +6,8 @@ import { PortableText } from '@portabletext/react';
 import ErrorPage from "../../404/page";
 import Link from "next/link";
 import Footer from "@/components/footer/footer";
+import BackButton from "@/components/UI/backbutton";
+import RegistrerButton from "@/components/UI/registrerbutton";
 
 // Props for the event page 
 type Props = {
@@ -55,11 +57,14 @@ export default async function PageForEvent({ params }: Props) {
           </div>
 
 
-          <Link href={!isOver ? event.url : "#"} target="_blank" className={`inline-flex items-center px-3 py-2 mt-10 text-sm font-medium text-center rounded-lg  focus:ring-4 focus:outline-none ${isOver ? "cursor-not-allowed bg-gray-500" : "bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 "
-            } `}>
-            Påmelding
-            <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-          </Link>
+          <div className="flex justify-left gap-2">
+
+            <RegistrerButton isEventOverBoolean={isOver ? true : false} urlToForm={event.url} />
+
+            <BackButton link="/" text="Tilbake" />
+
+          </div>
+          
 
         </div>
       </main>
