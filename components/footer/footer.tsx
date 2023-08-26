eaf2fe507b6efc3434da3fc156664802ba0134bc
KjetilMainPc <kjetilindrehus@gmail.com>
2023-08-26 13:45:50 +0200

    fix: remove lightmode for footer.

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer(){
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                     <span className="text-sm sm:text-center text-gray-400">© 2023 <Link href="https://startgjovik.no/" className="hover:underline">Start Gjøvik</Link>. Alle Rettigheter Reservert.
                     </span>
-                    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
                         <li>
                             <Link href={privacyPolicyURL} target="_blank" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
                         </li>
