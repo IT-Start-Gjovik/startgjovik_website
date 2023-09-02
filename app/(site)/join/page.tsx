8da678e76cf70d061801ad86987f293d72f0d2cd
Kjetil Indrehus <66110094+KjetilIN@users.noreply.github.com>
2023-09-02 15:53:45 +0200

    Create Join Page (#90)
    * add: new join page with title
    
    * add: link to join page

---
+++ app/(site)/join/page.tsx
@@ -0,0 +1,14 @@
+export default function JoinPage(){
+
+
+    return (
+        <main className="bg-gray-900 min-h-screen">
+
+            <div className="flex justify-center pt-10">
+                <h1 className="text-5xl">Bli medlem i Start Gjøvik 🚀</h1>
+            </div>
+            
+
+        </main>
+    );
+}
