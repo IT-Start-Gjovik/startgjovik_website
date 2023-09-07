3dd4327b5c84f1bf7d8e767981c08e9ba07475bf
Kjetil Indrehus <66110094+KjetilIN@users.noreply.github.com>
2023-09-07 12:18:10 +0200

    Merge v1.1.1 (#108)
    * fix: spelling mistake
    
    * fix: date time error (#104)
    
    * build(deps): bump actions/checkout from 3 to 4 (#100)
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 3 to 4.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v3...v4)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: lightmode join page
    
    * fix: lightmode for event card
    
    * fix: light mode for event list
    
    * fix: lightmode for checklist item
    
    * fix: lightmode for spinner
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- utils/date.ts
+++ utils/date.ts
@@ -1,15 +1,28 @@
 const getDateTimeFormat = (date: string) => {
     const eventDate: Date = new Date(date);
     const dateFormat: string =
-        eventDate.getDay() +
+        eventDate.getDate()+
         ". " +
-        eventDate.toLocaleString("no-NO", { month: "long" }) +
-        " " +
-        eventDate.getFullYear().toString();
+        eventDate.toLocaleString("no-NO", { month: "long" })
+
+    // Event minutes
+    let eventMinutes: number | string = eventDate.getMinutes();
+
+    // If it is 0, set it to "00" for fixing the bug
+    if(eventMinutes === 0){
+        eventMinutes = "00";
+    }else if(eventMinutes > 0 && eventMinutes < 10){
+        // For minutes that are f.exp :01 to :09
+        eventMinutes = "0" + eventMinutes.toString();
+    }else{
+        // it is 
+        eventMinutes.toString();
+    }
+
+
     const timeFormat: string =
         eventDate.getHours().toString() +
-        ":" +
-        eventDate.getMinutes().toString();
+        ":" + eventMinutes;
     return { dateFormat, timeFormat };
 };
 
