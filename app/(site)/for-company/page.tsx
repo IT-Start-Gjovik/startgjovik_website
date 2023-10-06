0da28688dbab93219a18a44649fdee920a174157
abdi <abdiraay@ntnu.no>
2023-10-06 21:32:24 +0200

    Added for-company route

---
+++ app/(site)/for-company/page.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/footer/footer';
+
+export default function ForCompany() {
+    return (
+        <div class="bg-gray-900 min-h-screen">
+        <main>
+            <h1 class="ml-4 mb-4 text-7git xl font-extrabold leading-none leading-snug text-center">For bedrifter</h1>
+            <p class="ml-4 mb-4 text-lg font-normal text-gray-500 dark:text-gray-400 text-center">Her skal vi legge til noe info om oss for bedriftene!</p>
+
+            <div class="absolute inset-x-0 bottom-0">
+                <Footer />
+            </div>
+        </main>
+        </div>
+    )
+}
