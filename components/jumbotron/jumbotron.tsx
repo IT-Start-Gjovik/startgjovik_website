729a09ead3c5daadd4e7033c02118eaf2ad3f996
Nahom101-1 <146176292+Nahom101-1@users.noreply.github.com>
2023-10-20 16:16:33 +0200

    jumbotron.tsx
    gjøre om-oss til en link og referert til om-oss siden

--- components/jumbotron/jumbotron.tsx
+++ components/jumbotron/jumbotron.tsx
@@ -12,9 +12,9 @@ export default function HeaderJumbotron() {
                         Bli med
                         <svg aria-hidden="true" className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </Link>
-                    <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-700 hover:bg-gray-700 focus:ring-4 focus:ring-gray-800">
-                        Les mer
-                    </a>
+                    <Link href="om-oss" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-700 hover:bg-gray-700 focus:ring-4 focus:ring-gray-800">
+                        om-oss
+                    </Link>
                 </div>
             </div>
         </section>
