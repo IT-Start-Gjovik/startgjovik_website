ac9afb799704167cf8abc756609f6c0e852c58be
abdi <abdiraay@ntnu.no>
2023-10-25 11:34:11 +0200

    base blender site

---
+++ app/(site)/blender/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/footer/footer';
+
+function blender() {
+    return (
+        <div className="bg-gray-900 min-h-screen">
+        <h1 className='ml-4 mb-4 text-7xl font-extrabold leading-none leading-snug text-center'>
+            Blender
+        </h1>
+        
+        <Footer />
+        </div>
+    );
+}
+
+export default blender;
