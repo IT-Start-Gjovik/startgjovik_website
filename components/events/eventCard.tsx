f8db1a7c90a362d403f2de03a5d6b107fdf96120
Mosazghi <116307580+Mosazghi@users.noreply.github.com>
2023-10-28 10:34:05 +0200

    fix(urgent?): aligns eventcards section properly (#166)
    * fix: aligns eventcards section properly
    
    * fix: remove image stretch
    
    * fix: remove image stretch
    
    * fix: further improvm.
    
    * fix: eventcards now align perfectly
    
    * fix: eventcards now align perfectly

--- components/events/eventCard.tsx
+++ components/events/eventCard.tsx
@@ -1,87 +1,70 @@
-import event from "@/backend/schemas/event";
-import Image from "next/image";
-import Link from "next/link";
-import { split } from "lodash";
-import getEnglishMonth from "@/utils/englishMonth";
+import event from '@/backend/schemas/event';
+import Image from 'next/image';
+import Link from 'next/link';
+import { split } from 'lodash';
+import getEnglishMonth from '@/utils/englishMonth';
 
-// Define the props interface for the EventCard component
+// Interface for the event card
 interface EventProps {
-  imageUrl: string;
-  title: string;
-  date?: string;
-  time?: string;
-  description: string;
-  slug: string;
+    imageUrl: string;
+    title: string;
+    date?: string;
+    time?: string;
+    description: string;
+    slug: string;
 }
 
-// Define the EventCard component
-const EventCard: React.FC<EventProps> = ({
-  date,
-  title,
-  imageUrl,
-  time,
-  description,
-  slug,
-}: EventProps) => {
-  
-  const [dayNum, monthNorwegian] = date ? split(date, ' ') : [null, null];
+// Event card itself
+const EventCard: React.FC<EventProps> = ({ date, title, imageUrl, time, description, slug }: EventProps) => {
+    const [dayNum, monthNorwegian] = date ? split(date, ' ') : [null, null];
 
-  const eventDateTime = date && time ? new Date(`${dayNum} ${(getEnglishMonth(monthNorwegian as any))} ${new Date().getFullYear()} ${time}`) : null;
-  const currentDateTime = new Date();
-  const eventFinished = eventDateTime && eventDateTime < currentDateTime;
-    
-  return (
-    <div className="flex flex-col items-center border rounded-lg shadow md:flex-row md:max-w-xl max-w-md border-gray-700 bg-gray-800 hover:bg-gray-700">
-      <Image
-        className="object-cover w-full rounded-t-lg h-96 md:h-48 md:w-48 md:rounded-none md:rounded-l-lg"
-        src={imageUrl}
-        alt="Project card"
-        width={400}
-        height={400}
-      />
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-          {title}
-        </h5>
-        <p className="mb-3 font-normal text-gray-400">{description}</p>
-        {eventFinished ? (
-          // Display this message if the event has finished
-          <div>
-            <p className="bg-red-100 text-red-800 text-sm font-medium mr-2 px-3 py-2 rounded-lg dark:bg-red-900 dark:text-red-200 inline">
-            ⌛️ Arrangementet er over!
-          </p>
-          </div>
-        ) : (
-          // Display event date and time if the event is ongoing
-          <div>
-            <p>📅 {date}</p>
-            <p>🕕 {time}</p>
+    const eventDateTime = date && time ? new Date(`${dayNum} ${getEnglishMonth(monthNorwegian as any)} ${new Date().getFullYear()} ${time}`) : null;
+    console.log(eventDateTime);
+    const currentDateTime = new Date();
+    const eventFinished = eventDateTime && eventDateTime < currentDateTime;
 
-            <Link
-          href={`/arrangementer/${slug}`}
-          className="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none  bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
-        >
-          Les mer
-          <svg
-            aria-hidden="true"
-            className="w-4 h-4 ml-2 -mr-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </Link>
-          </div>
-          
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className='max-w-sm w-full lg:max-w-full lg:flex lg:justify-stretch lg:items-stretch h-full rounded-lg shadow  bg-gray-800 hover:bg-gray-700'>
+            <div
+                className='h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden md:rounded-none md:rounded-l-lg'
+                style={{ backgroundImage: `url(${imageUrl})` }}
+                title='Event image'></div>
+            <div className='flex flex-col rounded-b lg:rounded-b-none lg:rounded-r p-4 justify-between leading-normal'>
+                <div className='mb-4'>
+                    <h5 className='mb-2 text-xl font-bold tracking-tight text-white'>{title}</h5>
+                    <p className='mb-3 font-normal text-gray-400'>{description}</p>
+                </div>
+                <div className='flex flex-col'>
+                    {eventFinished ? (
+                        <p className='bg-red-100 text-red-800 text-sm font-medium mr-2 px-3 py-2 rounded-lg dark:bg-red-900 dark:text-red-200 inline'>
+                            ⌛️ Arrangementet er over!
+                        </p>
+                    ) : (
+                        <>
+                            <h4> 📅 {date}</h4>
+                            <h4>🕕 {time}</h4>
+                            <Link
+                                href={`/arrangementer/${slug}`}
+                                className='inline-flex items-center w-full px-3 py-2 mt-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none  bg-blue-600 hover:bg-blue-700 focus:ring-blue-800'>
+                                Les meir{' '}
+                                <svg
+                                    aria-hidden='true'
+                                    className='w-4 h-4 ml-2 -mr-1'
+                                    fill='currentColor'
+                                    viewBox='0 0 20 20'
+                                    xmlns='http://www.w3.org/2000/svg'>
+                                    <path
+                                        fillRule='evenodd'
+                                        d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                                        clipRule='evenodd'></path>
+                                </svg>{' '}
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default EventCard;
