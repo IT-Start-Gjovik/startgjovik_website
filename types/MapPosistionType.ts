dcf64c745f0c54289ae456e97981e9729a65a691
abdi <abdiraay@ntnu.no>
2023-11-05 19:18:42 +0100

    change type definiton to mazemap

--- types/MapPosistionType.ts
+++ types/MapPosistionType.ts
@@ -8,20 +8,13 @@ export type MapPositionType = {
         buildingName: string;
         campusId: number;
         description: string | null;
-        externalReferenceTypes: Array<any>; // Replace 'any' with a more specific type if possible
+        externalReferenceTypes: Array<any>;
         floorId: number;
         floorName: string;
         id: number;
         identifier: string;
-        images: Array<any>; // Replace 'any' with a more specific type if possible
-        infoUrl: string | null;
-        infoUrlText: string | null;
         names: Array<string>;
-        poiId: number;
         title: string;
-        types: Array<{
-            // define the structure of the objects in this array if possible
-        }>;
         zLevel: number;
     };
 }
