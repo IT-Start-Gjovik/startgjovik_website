9d33ee65362eca62182918d1d5ed755e2abd25c0
Mosazghi <fest121@hotmail.com>
2023-11-08 11:15:59 +0100

    chore: header text bold + url carbon badge

--- components/sustainability/carbonFootprint/carbonFootprint.tsx
+++ components/sustainability/carbonFootprint/carbonFootprint.tsx
@@ -4,7 +4,7 @@ import { WebsiteCarbonBadge } from 'react-websitecarbon-badge';
 export default function CarbonFootprint() {
     return (
         <section>
-            <h2 className='text-3xl md:text-5xl md:text-center'>Carbon Footprint</h2>
+            <h2 className='text-3xl md:text-5xl md:text-center font-semibold'>Carbon Footprint</h2>
             <div className='mt-5 flex flex-col md:flex-row gap-10'>
                 <article className='bg-slate-900 hover:scale-110 hover:my-1 duration-700 p-5 rounded-lg shadow-2xl shadow-slate-700'>
                     <p className='text-[16.5px] '>
@@ -13,7 +13,7 @@ export default function CarbonFootprint() {
                     </p>
                 </article>
                 <div className='min-w-fit'>
-                    <WebsiteCarbonBadge dark={true} co2='0.10' percentage='90' />
+                    <WebsiteCarbonBadge dark={true} url='https://www.startgjovik.no/' />
                 </div>
             </div>
         </section>
