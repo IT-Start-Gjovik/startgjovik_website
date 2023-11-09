85a86638427ddf63efc6e06f4307f86929ea1f5b
hannah-sofie <hannah-sofie@live.no>
2023-11-09 12:52:31 +0100

    dynamic linking

--- components/events/eventBox.tsx
+++ components/events/eventBox.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+// Typescript interface for props passed to the EventBox component
 type EventBoxProps = {
     id: string;
     imageUrl: string;
@@ -9,23 +10,35 @@ type EventBoxProps = {
     link: string;
 };
 
+// This component displays a brief overview of an event (like a card)
 const EventBox: React.FC<EventBoxProps> = ({ imageUrl, title, alt, link }) => {
     return (
         <div className='flex flex-col items-center py-4 px-2 md:px-10 bg-gray-800 rounded-md shadow-lg max-w-xs mx-16 md:mx-0'>
-            <Image src={imageUrl} alt={alt} width={150} height={150} className='rounded-md mt-6 md:mt-10 mb-4 md:mb-8' />
+            {/* Image component for event thumbnail */}
+            <Image
+                src={imageUrl}
+                alt={alt}
+                width={150}
+                height={150}
+                sizes='(max-width: 768px) 100px, (max-width: 1200px) 150px, 200px'
+                className='rounded-md mt-6 md:mt-10 mb-4 md:mb-8'
+            />
 
+            {/* Event title */}
             <h3 className='mt-2 text-lg md:text-xl text-gray-200 hover:text-gray-400 transition duration-300 text-center w-full'>{title}</h3>
 
+            {/* Link to the event's detailed page */}
             <Link href={link} passHref>
                 <button className='mt-2 md:mt-4 mb-3 md:mb-5 px-3 md:px-4 py-1 md:py-2 bg-blue-700 text-gray-200 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-900 transition duration-300 flex items-center'>
-                    Les mer
+                    Les mer {/* "Read more" in Norwegian */}
+                    {/* Arrow icon indicating action */}
                     <svg
                         aria-hidden='true'
                         className='ml-1 md:ml-2 -mr-0.5 md:-mr-1 w-4 md:w-5 h-4 md:h-5'
                         fill='currentColor'
                         viewBox='0 0 20 20'
                         xmlns='http://www.w3.org/2000/svg'>
-                        <title>Arrow</title>
+                        <title>Arrow</title> {/* Title for the SVG for accessibility purposes */}
                         <path
                             fillRule='evenodd'
                             d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
--- components/events/eventData.tsx
+++ components/events/eventData.tsx
@@ -1,19 +1,25 @@
+// Base URL for images (will replace later)
 const BASE_IMAGE_URL = '/images/startLogo.png';
+// Base URL for events, used to construct the link for each event
 const BASE_EVENT_URL = '/alle-arrangementer';
 
+// Array of objects representing events
 export const events = [
+    // Event object structure
     {
-        id: '1',
+        id: '1', // Unique identifier for the event
         imageUrl: BASE_IMAGE_URL,
         title: 'BCM',
         alt: 'BCM',
-        link: `${BASE_EVENT_URL}/bcm`,
+        description: 'Beskrivelse av arrangementet her.',
+        link: `${BASE_EVENT_URL}/bcm`, // URL for the event detail page
     },
     {
         id: '2',
         imageUrl: BASE_IMAGE_URL,
         title: 'Casebreaker',
         alt: 'casebreaker',
+        description: 'Beskrivelse av arrangementet her.',
         link: `${BASE_EVENT_URL}/casebreaker`,
     },
     {
@@ -21,6 +27,7 @@ export const events = [
         imageUrl: BASE_IMAGE_URL,
         title: 'Hvordan starte bedrift fra A til Å',
         alt: 'hvordan starte bedrift fra a til å',
+        description: 'Beskrivelse av arrangementet her.',
         link: `${BASE_EVENT_URL}/hvordan-starte-bedrift-fra-a-til-aa`,
     },
     {
@@ -28,6 +35,7 @@ export const events = [
         imageUrl: BASE_IMAGE_URL,
         title: 'Idèkonkurranse',
         alt: 'idekonkurranse',
+        description: 'Beskrivelse av arrangementet her.',
         link: `${BASE_EVENT_URL}/idekonkurranse`,
     },
     {
@@ -35,6 +43,7 @@ export const events = [
         imageUrl: BASE_IMAGE_URL,
         title: 'Møt en gründer',
         alt: 'møt en grunder',
+        description: 'Beskrivelse av arrangementet her.',
         link: `${BASE_EVENT_URL}/mot-en-grunder`,
     },
     {
@@ -42,6 +51,7 @@ export const events = [
         imageUrl: BASE_IMAGE_URL,
         title: 'Andre arrangementer',
         alt: 'andre arrangementer',
+        description: 'Beskrivelse av arrangementet her.',
         link: `${BASE_EVENT_URL}/andre-arrangementer`,
     },
 ];
