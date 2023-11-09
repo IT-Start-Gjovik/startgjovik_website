ff91c7304792abd08f76961b3c9ebeac8cd84046
Mosazghi <116307580+Mosazghi@users.noreply.github.com>
2023-11-09 11:52:26 +0100

    sustainability bold and url badge (#219)
    * add: sustainability images and description
    
    * testing
    
    * add: everything about carbon footprint + some styling page
    
    * minor: component rename
    
    * chore: header text bold + url carbon badge

--- components/sustainability/sustainabilityText.tsx
+++ components/sustainability/sustainabilityText.tsx
@@ -1,7 +1,7 @@
 export default function SustainabilityText() {
     return (
         <section>
-            <h1 className='text-5xl md:text-7xl md:text-center'>Bærekraft</h1>
+            <h1 className='text-5xl md:text-7xl md:text-center font-bold'>Bærekraft</h1>
             <article className='mt-5 bg-slate-900 p-5 rounded-lg hover:scale-110 hover:mt-16  duration-700 shadow-2xl shadow-slate-700'>
                 <p className='text-lg md:text-xl'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum magna erat, at accumsan est finibus quis. Fusce dapibus viverra
