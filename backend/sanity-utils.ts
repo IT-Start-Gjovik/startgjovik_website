c4ca210a77d6a21b5072449baa7c806bb4f705fe
hannah-sofie <hannah-sofie@live.no>
2023-11-14 22:08:52 +0100

    hopefully fixed deployment errors

--- backend/sanity-utils.ts
+++ backend/sanity-utils.ts
@@ -1,12 +1,13 @@
-import React from 'react';
-import { createClient, groq } from "next-sanity";
-import { EventCardType } from "@/types/EventCardType";
+import { createClient, groq } from 'next-sanity';
+import { EventCardType } from '@/types/EventCardType';
+import { EventPageType } from '@/types/EventPageType';
+import { VervType } from '@/types/Verv';
 
 export async function getEventCards(): Promise<EventCardType[]> {
     const client = createClient({
-        projectId: "a42ubgcg",
-        dataset: "production",
-        apiVersion: "2023-07-06",
+        projectId: 'a42ubgcg',
+        dataset: 'production',
+        apiVersion: '2023-07-06',
         useCdn: false,
     });
 
@@ -21,15 +22,15 @@ export async function getEventCards(): Promise<EventCardType[]> {
             "image": image.asset -> url,
             "slug": slug.current,
         }`,
-        { currentDate }
+        { currentDate },
     );
 }
 
 export async function getPastEventCards(): Promise<EventCardType[]> {
     const client = createClient({
-        projectId: "a42ubgcg",
-        dataset: "production",
-        apiVersion: "2023-07-06",
+        projectId: 'a42ubgcg',
+        dataset: 'production',
+        apiVersion: '2023-07-06',
         useCdn: false,
     });
 
@@ -44,7 +45,46 @@ export async function getPastEventCards(): Promise<EventCardType[]> {
             "image": image.asset -> url,
             "slug": slug.current,
         }`,
-        { currentDate }
+        { currentDate },
     );
 }
 
+export async function getCurrentEventCards(slug: string): Promise<EventPageType> {
+    const client = createClient({
+        projectId: 'a42ubgcg',
+        dataset: 'production',
+        apiVersion: '2023-07-06',
+        useCdn: false,
+    });
+
+    return client.fetch(
+        groq`*[_type == "event" && slug.current == $slug][0]{
+            _id,
+            title,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content,
+            datetime
+        }`,
+        { slug },
+    );
+}
+
+export async function getVervs(): Promise<VervType[]> {
+    const client = createClient({
+        projectId: 'a42ubgcg',
+        dataset: 'production',
+        apiVersion: '2023-07-06',
+        useCdn: false,
+    });
+
+    return client.fetch(
+        groq`*[_type == "verv"]{
+            _id,
+            title,
+            url,
+            type
+        }`,
+    );
+}
