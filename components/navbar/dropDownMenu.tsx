147bfbd8042c0547d12231b482d1ee9c0576d467
fredsu <fredsu@stud.ntnu.no>
2023-11-14 22:30:19 +0100

    add dropdown menu and mobile responive

---
+++ components/navbar/dropDownMenu.tsx
@@ -0,0 +1,58 @@
+import { Fragment, ReactNode } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+type BurgerMenuProps = {
+    menuItems: { name: string, path: string }[]
+    title: string
+    menuIcon: ReactNode
+}
+
+
+export default function DropDownMenu({ menuItems, title, menuIcon} : BurgerMenuProps) {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md">
+            {title}
+            {menuIcon}
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            {menuItems.map((item, index) => (
+                <Menu.Item key={index}>
+                    {({ active }) => (
+                        <a
+                            href={item.path}
+                            className={classNames(
+                                active ? 'bg-white text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                            )}
+                        >
+                            {item.name}
+                        </a>
+                    )}
+                </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
