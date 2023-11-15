100c283d7664793241d50db37603b85c3e7fc341
fredsu <fredsu@stud.ntnu.no>
2023-11-15 15:10:04 +0100

    linitng navbar page

--- components/navbar/page.tsx
+++ components/navbar/page.tsx
@@ -14,12 +14,12 @@ export default function Navbar() {
         { name: 'Bærekraft', path: '/baerekraft' },
         { name: 'For Bedrifter', path: '/for-bedrifter' },
         { name: 'Personvern', path: '/personvern' },
-      ];
+    ];
 
     const eventsMenuItems = [
         { name: 'Kommende', path: '/' },
         { name: 'Tidligere', path: '/about' },
-      ];
+    ];
 
     const defualtMenuIcon = <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
 
@@ -29,20 +29,20 @@ export default function Navbar() {
         <div className="bg-darkblue">
             <nav className="flex items-center justify-between py-4 phone:pb-0 phone:pt-1 phone:pl-3 pl-5 text-white text-2xl font-semibold">
                 <div className='tablet:hidden desktop:hidden'>
-                    <DropDownMenu menuItems={homePageMenuItems} title="" menuIcon={burgerMenuIcon} textSize='text-2xl'/>
+                    <DropDownMenu menuItems={homePageMenuItems} title="" menuIcon={burgerMenuIcon} textSize='text-2xl' />
                 </div>
                 <ul className="flex space-x-7 -space-y-0 tracking-tight leading-none">
                     {homePageMenuItems.map((item, index) => (
-                    <li key={index} className='phone:hidden'>
-                        <Link key={index} href={item.path} className={`link ${pathname === item.path ? 'active-link' : ''} `}>{item.name}</Link>
-                    </li>
+                        <li key={index} className='phone:hidden'>
+                            <Link key={index} href={item.path} className={`link ${pathname === item.path ? 'active-link' : ''} `}>{item.name}</Link>
+                        </li>
                     ))}
 
                     <li className='phone:hidden'>
-                         <DropDownMenu menuItems={eventsMenuItems} title="Arrangementer" menuIcon={defualtMenuIcon} textSize='text-lg'/>
+                        <DropDownMenu menuItems={eventsMenuItems} title="Arrangementer" menuIcon={defualtMenuIcon} textSize='text-lg' />
                     </li>
                 </ul>
-            </nav>       
+            </nav>
         </div>
     )
 }
