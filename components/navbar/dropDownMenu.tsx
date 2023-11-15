ba57717e5b7c104cf21b2f0668a2864d62b5dbfd
Fredrik Sundt-Hansen <38140980+FredrikSundt-Hansen@users.noreply.github.com>
2023-11-15 15:29:57 +0100

    Front page nav bar (#187)
    * add first version of figma of nav bar
    
    * remove figma form github, add to google docs
    
    * add hader with nav bar
    
    add new color 'darkblue' Add new CSS element called 'active-link' when a page is active in navbar
    
    * add dropdown menu and mobile responive
    
    * change desktop to not have max size
    
    * add on hover effect on drop down menu
    
    change burger menu icon size
    
    * add text size as parameter to drop down menu
    
    * change naming of BurgerMenuProps to MenuProps
    
    * add key to li and a element
    
    * add small documentation
    
    * made navbar text semibold
    
    * resolve merge conflict with development
    
    * resolve merge conflict with development

---
+++ components/navbar/dropDownMenu.tsx
@@ -0,0 +1,57 @@
+import { Fragment, ReactNode } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+type MenuProps = {
+    menuItems: { name: string, path: string }[]
+    title: string
+    menuIcon: ReactNode
+    textSize: string
+}
+
+
+export default function DropDownMenu({ menuItems, title, menuIcon, textSize} : MenuProps) {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md">
+            {title}
+            {menuIcon}
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute left-0 right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            {menuItems.map((item, index) => (
+                <Menu.Item key={index}>
+                    {({ active }) => (
+                        <a key={index}
+                          href={item.path}
+                          className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'block px-4 py-2 leading-none ' + textSize
+                          )}
+                        >
+                            {item.name}
+                        </a>
+                    )}
+                </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
---
+++ components/navbar/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import DropDownMenu from './dropDownMenu';
+import Image from 'next/image'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+export default function Navbar() {
+    const pathname = usePathname()
+
+    const homePageMenuItems = [
+        { name: 'Hjem', path: '/' },
+        { name: 'Bærekraft', path: '/baerekraft' },
+        { name: 'For Bedrifter', path: '/for-bedrifter' },
+        { name: 'Personvern', path: '/personvern' },
+      ];
+
+    const eventsMenuItems = [
+        { name: 'Kommende', path: '/' },
+        { name: 'Tidligere', path: '/about' },
+      ];
+
+    const defualtMenuIcon = <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+
+    const burgerMenuIcon = <Image src="/images/burger-menu.png" alt="Menu button" width={50} height={50} />
+
+    return (
+        <div className="bg-darkblue">
+            <nav className="flex items-center justify-between py-4 phone:pb-0 phone:pt-1 phone:pl-3 pl-5 text-white text-2xl font-semibold">
+                <div className='tablet:hidden desktop:hidden'>
+                    <DropDownMenu menuItems={homePageMenuItems} title="" menuIcon={burgerMenuIcon} textSize='text-2xl'/>
+                </div>
+                <ul className="flex space-x-7 -space-y-0 tracking-tight leading-none">
+                    {homePageMenuItems.map((item, index) => (
+                    <li key={index} className='phone:hidden'>
+                        <Link key={index} href={item.path} className={`link ${pathname === item.path ? 'active-link' : ''} `}>{item.name}</Link>
+                    </li>
+                    ))}
+
+                    <li className='phone:hidden'>
+                         <DropDownMenu menuItems={eventsMenuItems} title="Arrangementer" menuIcon={defualtMenuIcon} textSize='text-lg'/>
+                    </li>
+                </ul>
+            </nav>       
+        </div>
+    )
+}
