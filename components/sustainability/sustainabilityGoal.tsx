81b553d9c20b33f2cec6bbdc692951a123aefd6a
Mosazghi <fest121@hotmail.com>
2023-11-17 23:42:53 +0100

    fix: some minor fixes with styling etc

--- components/sustainability/sustainabilityGoal.tsx
+++ components/sustainability/sustainabilityGoal.tsx
@@ -10,14 +10,14 @@ interface SustainabilityImageProps {
 
 export default function SustainabilityGoal({ title, description, imageSrc, linkUrl }: SustainabilityImageProps) {
     return (
-        <div className='w-fit m-auto sm:m-0 flex flex-col items-center justify-center hover:scale-105 duration-500 border-b border-l border-r border-solid rounded-2xl p-1'>
+        <div className='w-fit m-auto flex flex-col items-center justify-center hover:scale-105 duration-500 border-b border-l border-r border-solid rounded-2xl py-1'>
             <Link href={linkUrl} target='_blank' rel='noopener noreferrer' aria-label={`Read more about ${title}`}>
                 <div className='relative overflow-hidden rounded-xl mb-2 max-w-fit'>
                     <Image src={imageSrc} width={280} height={280} alt={`Bilde av bærekraftsmål: ${title}`} className=' transition-opacity duration-1000' />
                     <div className='absolute inset-0 bg-black bg-opacity-0 hover:bg-opacity-10 transition-opacity duration-1000'></div>
                 </div>
             </Link>
-            <p className='flex-1 max-w-[280px] text-center md:text-start mx-1 overflow-x-clip text-lg'>{description}</p>
+            <p className='flex-1 max-w-[280px] text-center mx-1 overflow-x-clip text-lg'>{description}</p>
         </div>
     );
 }
--- components/sustainability/sustainabilityText.tsx
+++ components/sustainability/sustainabilityText.tsx
@@ -1,9 +1,9 @@
 export default function SustainabilityText() {
     return (
         <section>
-            <h1 className='text-5xl md:text-7xl md:text-center font-bold'>Bærekraft</h1>
-            <article className='mt-5 bg-slate-900 p-5 rounded-lg hover:scale-110 hover:mt-16  duration-700 shadow-2xl shadow-slate-700'>
-                <p className='text-lg md:text-xl'>
+            <h1 className='text-5xl desktop:text-7xl phone:text-start text-center font-bold'>Bærekraft</h1>
+            <article className='mt-5 bg-slate-900 p-5 rounded-lg hover:scale-110 hover:mt-8 duration-1000 shadow-2xl shadow-slate-700'>
+                <p className='text-lg desktop:text-xl'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum magna erat, at accumsan est finibus quis. Fusce dapibus viverra
                     arcu, vel mollis purus commodo nec. Phasellus nulla nulla, accumsan ac massa quis, tempor sollicitudin elit. Suspendisse facilisis risus
                     tincidunt eros ultrices, egestas posuere ante lacinia. Etiam interdum commodo eros ac mollis. Integer interdum, eros vitae aliquet
