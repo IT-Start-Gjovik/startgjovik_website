8fc2c9148a40a347e8f72f1fe628bb953cba750c
Mosazghi <fest121@hotmail.com>
2023-11-22 00:12:24 +0100

    new: complete text for sutainability page

--- components/sustainability/sustainabilityText.tsx
+++ components/sustainability/sustainabilityText.tsx
@@ -1,14 +1,19 @@
+import Link from 'next/link';
+
 export default function SustainabilityText() {
     return (
         <section>
-            <h1 className='text-5xl desktop:text-7xl phone:text-start text-center font-bold'>Bærekraft</h1>
+            <h1 className='text-5xl md:text-7xl md:text-center font-bold'>Bærekraft</h1>
             <article className='mt-5 bg-slate-900 p-5 rounded-lg hover:scale-110 hover:mt-8 duration-1000 shadow-2xl shadow-slate-700'>
-                <p className='text-lg desktop:text-xl'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum magna erat, at accumsan est finibus quis. Fusce dapibus viverra
-                    arcu, vel mollis purus commodo nec. Phasellus nulla nulla, accumsan ac massa quis, tempor sollicitudin elit. Suspendisse facilisis risus
-                    tincidunt eros ultrices, egestas posuere ante lacinia. Etiam interdum commodo eros ac mollis. Integer interdum, eros vitae aliquet
-                    dignissim, arcu nulla volutpat dolor, sed cursus metus nibh consectetur enim. Ut lobortis turpis non posuere volutpat. Ut ullamcorper massa
-                    at eros pharetra, vel pellentesque nisi tempor.
+                <p className='text-lg md:text-xl'>
+                    Vi tror på kraften i ungdommelig kreativitet og engasjement for å forme en mer bærekraftig fremtid. Vår dedikasjon til bærekraft reflekteres
+                    i hvordan vi tilnærmer oss alle våre prosjekter og initiativer, alltid med mål om å støtte og oppnå FNs bærekraftsmål. Gjennom våre
+                    aktiviteter, som blant annet,{' '}
+                    <Link href='/alle-arrangementer/casebreaker' target='_blank' className=' text-blue-400'>
+                        casebreaker
+                    </Link>
+                    , foredrag og nettverksevents, inspirerer vi studenter til å utvikle og realisere ideer som ikke bare fremmer økonomisk vekst, men også
+                    ivaretar miljømessige og sosiale hensyn.
                 </p>
             </article>
         </section>
