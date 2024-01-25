9361cd1fd288aee003a1355649e2192bdf05fa4a
Mosazghi <fest121@hotmail.com>
2024-01-25 22:37:29 +0100

    fix: remove custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    screens: {
-      'sm': {'max': '767px'},
-
-      'md': {'min': '768px', 'max': '1023px'},
-
-      'lg': {'min': '1024px', 'max': '1279px'},
-
-      'xl': {'min': '1280px', 'max': '1535px'},
-
-      '2xl': {'min': '1536px'},
-    },
-    extend: {
-      colors: {
-        'gradient-start': 'rgb(0, 77, 123)',
-        'gradient-mid': 'rgb(1, 22, 43)',
-        'gradient-end': 'rgb(194, 217, 239)',
-        'darkblue': 'rgb(1, 26, 49)',
+  content :
+          [
+            './pages/**/*.{js,ts,jsx,tsx,mdx}',
+            './components/**/*.{js,ts,jsx,tsx,mdx}',
+            './app/**/*.{js,ts,jsx,tsx,mdx}',
+          ],
+  theme : {
+    extend : {
+      colors : {
+        'gradient-start' : 'rgb(0, 77, 123)',
+        'gradient-mid' : 'rgb(1, 22, 43)',
+        'gradient-end' : 'rgb(194, 217, 239)',
+        'darkblue' : 'rgb(1, 26, 49)',
       },
     },
   },
-  plugins: [],
+  plugins : [],
 }
