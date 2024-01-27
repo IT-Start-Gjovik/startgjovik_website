f8a4322d5a6811cdddbe602191f95b8ca42d7aa9
Nahom101-1 <nahomberhane101@gmail.com>
2024-01-27 13:56:50 +0100

    laget seprat componenter og rydet litt opp

--- app/(site)/om-oss/page.tsx
+++ app/(site)/om-oss/page.tsx
@@ -1,21 +1,26 @@
+
 import BackButton from '@/components/UI/backbutton';
+import Logo from '@/components/UI/logo';
 import Footer from '@/components/footer/footer';
+import Header from '@/components/header/page';
+import {Omstart }from '@/components/omstart/omstarttekst';
+import { Startstyre } from '@/components/startstyre/styre';
+
 
 export default function AboutUsPage() {
+    
     return (
         <main className='bg-gray-900 min-h-screen'>
-            <div className='flex justify-center pt-10 px-6'>
-                <h1 className='text-4xl md:text-6xl'> Om oss</h1>
-            </div>
-            <h2 className='text-2xl md:text-6xl'>Kommer snart....</h2>
-            <p> Her vil det stå mer om start....</p>
+            <Header />
+            <Logo />
+            <Omstart />
+            <Startstyre />
             <div className='fixed top-0 right-0 m-4'>
-                <BackButton link='/' text='Tilbake til hovedsiden' />
+                <BackButton link='/' text='Tilbake' />
             </div>
-            <footer
-                className='w-full fixed bottom-0 '>
-                <Footer />
-            </footer>
+            <Footer />
         </main>
     );
 }
+
+
---
+++ types/StyreType.ts
@@ -0,0 +1,10 @@
+export type StyreType = {
+    alt: string | undefined;
+    _id: string;
+    title: string;
+    image: string;
+    slug: string;
+    url: string;
+    stilling: string;
+    styre: Array<string>;
+};
