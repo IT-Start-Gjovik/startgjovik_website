7cdf81695c997687aa0134ab55a5f90a137629d2
AbdiYus <yusabdirahman@gmail.com>
2024-02-24 16:00:44 +0100

    stuff

--- app/(site)/for-bedrifter/page.tsx
+++ app/(site)/for-bedrifter/page.tsx
@@ -8,14 +8,17 @@ import ForCompanyMap from '@/components/for-company/forCompanyMap';
 
 export default function ForCompany() {
     return (
-        <div className='min-w-screen-lg mx-auto'> 
-        <Header />
-        <main>
-            <ForCompanyHero />
-            <ForCompanyInfo />
-            <ForCompanyMap />
-            <Footer />
-        </main>
+        <div className='min-w-screen-lg mx-auto bg-[#132D4E]'> 
+            <Header />
+            <main>
+                <div>
+                    <ForCompanyHero />
+                </div>
+                
+                {/* <ForCompanyInfo />
+                <ForCompanyMap /> */}
+                <Footer />
+            </main>
         </div>
     );
 }
