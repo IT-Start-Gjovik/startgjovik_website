6cb47507070f97981516ee229e56c09bbb0f0309
AbdiYus <yusabdirahman@gmail.com>
2024-02-27 20:35:01 +0100

    added new style

--- app/(site)/for-bedrifter/page.tsx
+++ app/(site)/for-bedrifter/page.tsx
@@ -4,21 +4,27 @@ import Header from '@/components/header/page';
 import ForCompanyHero from '@/components/for-company/forCompanyHero';
 import ForCompanyInfo from '@/components/for-company/forCompanyInfo';
 import ForCompanyMap from '@/components/for-company/forCompanyMap';
+import Hero from '@/components/hero/hero';
 
+const forCompanyProps = {
+    title: 'For bedrifter',
+    color: 'white',
+    textColor: 'black',
+    imageSrc: '/images/for-company/Front.png',
+    logo: false,
+    content: <ForCompanyHero />,
+}
 
 export default function ForCompany() {
     return (
         <div className='min-w-screen-lg mx-auto bg-[#132D4E]'> 
             <Header />
             <main>
-                <div>
-                    <ForCompanyHero />
-                </div>
-                
+                <Hero {...forCompanyProps} />
                 {/* <ForCompanyInfo />
                 <ForCompanyMap /> */}
-                <Footer />
             </main>
+            <Footer />
         </div>
     );
 }
--- components/for-company/forCompanyHero.tsx
+++ components/for-company/forCompanyHero.tsx
@@ -1,32 +1,39 @@
-import Image from "next/image"
-import Frontprop from "../frontPage/frontProp"
+
 
 export default function ForCompanyHero() {
     return (
         <>
-            <div className="relative">
-                {/* Introduction */}
-                <Frontprop path="/images/for-company/Front.png" color="#ffffff" title="For bedrifter"/>
-                <div
-                className='w-full absolute bottom-0'
-                style={{
-                    height: '35%',
-                }}>
-                    <div className='flex flex-col lg:mt-[150px] mt-[110px] items-center h-[500px] bg-white'>
-                        <h1 className='text-black text-center font-bold text-[40px]'>
-                            Hva får du ut av et samarbeid med oss?
-                        </h1>
-                        <p className="text-black w-auto">
-                            Start Gjøvik tilbyr bedrifter og organisasjoner muligheten til å samarbeide 
-                            med engasjerte studenter fra NTNU Gjøvik. Dette gir dere muligheten til å utvikle 
-                            nye fremtidige medarbeidere og samarbeidspartnere til deres bedrift eller organisasjon.!
-                        </p>
-                    </div>
-            </div>
-            </div>
+        <h1 className='text-2xl md:text-4xl font-extrabold pt-5 px-5 '>
+            Hva får du ut av et samarbeid med oss?
+        </h1>
+        <p className="text-[16px] mt-2 md:text-xl">
+        Start Gjøvik tilbyr bedrifter og organisasjoner muligheten til å samarbeide 
+        med engasjerte studenter fra NTNU Gjøvik. Dette gir dere muligheten til å utvikle 
+        nye fremtidige medarbeidere og samarbeidspartnere til deres bedrift eller organisasjon.!
+        </p>
         </>
     )
 }
+
+{/* <div className="relative">
+<Frontprop path="/images/for-company/Front.png" color="#ffffff" title="For bedrifter"/>
+<div
+className='w-full absolute bottom-0'
+style={{
+    height: '35%',
+}}>
+    <div className='flex flex-col lg:mt-[150px] mt-[110px] items-center h-[500px] bg-white'>
+        <h1 className='text-black text-center font-bold text-[40px]'>
+            Hva får du ut av et samarbeid med oss?
+        </h1>
+        <p className="text-black w-auto">
+            Start Gjøvik tilbyr bedrifter og organisasjoner muligheten til å samarbeide 
+            med engasjerte studenter fra NTNU Gjøvik. Dette gir dere muligheten til å utvikle 
+            nye fremtidige medarbeidere og samarbeidspartnere til deres bedrift eller organisasjon.!
+        </p>
+    </div>
+</div>
+</div> */}
 {/* <div className='flex justify-center h-fit mb-3'>
                 <div className='grid w-4/5 sm:grid-cols-1 xl:grid-cols-2 h-auto p-7 shadow-slate-700 bg-slate-900 rounded-xl shadow-2xl xl:justify-self-start'>
                     <div className='flex items-center justify-center rounded-lg relative h-auto'>
