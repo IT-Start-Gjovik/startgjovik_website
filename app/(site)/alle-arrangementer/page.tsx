815a275ed25eeff7465ff35e6dd5980931b3c1bc
hannah-sofie <hannah-sofie@live.no>
2024-03-02 18:05:04 +0100

    changed all events page design

--- app/(site)/alle-arrangementer/page.tsx
+++ app/(site)/alle-arrangementer/page.tsx
@@ -4,11 +4,21 @@ import { getPastEventCards } from '@/backend/sanity-utils';
 import BackButton from '@/components/UI/backbutton';
 import EventBox from '@/components/events/eventBox';
 import { events } from '@/components/events/eventData';
-import PastEventBox from '@/components/events/pastEventBox';
-import SectionTitle from '@/components/events/sectionTitle';
 import Footer from '@/components/footer/footer';
 import { EventCardType } from '@/types/EventCardType';
 import React, { useEffect, useState } from 'react';
+import Header from '@/components/header/page';
+import Hero from '@/components/hero/hero';
+import EventHeroContent from '@/components/events/eventHeroContent';
+
+const eventHeroProps = {
+    title: 'Våre arrangementer',
+    color: '#f5f5f5',
+    textColor: 'black',
+    imageSrc: '/images/events/BCM.png',
+    logo: false,
+    content: <EventHeroContent />,
+};
 
 export default function AllEventsPage() {
     const [pastEvents, setPastEvents] = useState<EventCardType[]>([]);
@@ -27,44 +37,20 @@ export default function AllEventsPage() {
     }, []);
 
     return (
-        <main className='bg-gradient-to-r from-gray-800 via-gray-900 to-black min-h-screen flex flex-col items-center justify-start'>
-            <header>
-                <SectionTitle title='Våre arrangementer' />
-            </header>
-
-            <section className='grid md:grid-cols-3 gap-8 my-8 w-full max-w-5xl'>
-                {events.map((event) => (
-                    <EventBox key={event.id} {...event} />
-                ))}
-            </section>
-
-            <section>
-                <SectionTitle title='Tidligere arrangementer' />
-                {/* Change to a single-column grid layout */}
-                <div className='grid grid-cols-1 gap-8 my-8 w-full max-w-5xl'>
-                    {pastEvents.length > 0 ? (
-                        pastEvents.map((event) => (
-                            <PastEventBox
-                                key={event._id}
-                                id={event._id}
-                                title={event.title}
-                                alt={`Image for ${event.title}`}
-                                link={`/arrangementer/${event.slug}`}
-                            />
-                        ))
-                    ) : (
-                        <p className='mt-2 text-center'>Ingen tidligere arrangementer.</p>
-                    )}
+        <div className='bg-[#f5f5f5]'>
+            <Header />
+            <main>
+                <Hero {...eventHeroProps} />
+                <section className='grid md:grid-cols-3 gap-8 my-8 w-full max-w-5xl mx-auto px-4 md:px-0'>
+                    {events.map((event) => (
+                        <EventBox key={event.id} {...event} />
+                    ))}
+                </section>
+                <div className='fixed top-0 right-0 m-4'>
+                    <BackButton link='/' text='Tilbake til hovedsiden' />
                 </div>
-            </section>
-
-            <div className='fixed top-0 right-0 m-4'>
-                <BackButton link='/' text='Tilbake til hovedsiden' />
-            </div>
-
-            <footer className='w-full'>
-                <Footer />
-            </footer>
-        </main>
+            </main>
+            <Footer />
+        </div>
     );
 }
