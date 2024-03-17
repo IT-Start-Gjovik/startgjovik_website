f6a1e173fd5ff97d063a27b1ae839f6bf74e54dd
hannah-sofie <hannah-sofie@live.no>
2024-03-17 22:00:28 +0100

    working on the redsign for event pages

--- components/events/sectionTitle.tsx
+++ components/events/sectionTitle.tsx
@@ -7,13 +7,9 @@ type SectionTitleProps = {
 const SectionTitle: React.FC<SectionTitleProps> = ({ title }) => (
     <>
         <div className='pt-10 px-6 text-center'>
-            <h1 className='text-4xl md:text-6xl text-white leading-tight font-bold'>{title}</h1>
-            <div className='flex justify-center items-center mt-4 mb-8'>
-                <span className='text-blue-500 mr-2'>✦</span> {/* Decorative element */}
-                {/* Full-width hr with gradient */}
-                <hr className='w-full h-1 bg-gradient-to-r from-purple-300 via-pink-500 to-blue-500 border-0 rounded-full' />
-                <span className='text-blue-500 ml-2'>✦</span> {/* Decorative element */}
-            </div>
+            <h1 className='text-4xl md:text-6xl text-[#B2C51F] leading-tight font-bold'>
+                {title}
+            </h1>
         </div>
     </>
 );
