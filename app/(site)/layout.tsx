432b4170e6ba406c87532200ce2df687a6c96ad9
Nahom Berhane <nahomba@stud.ntnu.no>
2025-01-15 15:25:40 +0100

    Updated layout.tsx, added Vercel Analytics and Speed Insights, handled ESLint conflict

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,8 @@
 import Footer from '@/components/footer/footer';
 import Header from '@/components/header/page';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,12 +18,14 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+      <html lang='en'>
+          <body className={inter.className}>
+              <Header />
+              {children}
+              <Footer />
+              <Analytics />
+              <SpeedInsights />
+          </body>
+      </html>
   );
 }
